68b96799d4ce2c314617be3bc2d16dbad2872b2d
Hugo do Carmo <hugrito.kun@gmail.com>
2019-06-26 20:25:06 -0300

    add the class Finder
    - This class is used to get loaded classes in your application;

---
+++ src/Finder.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace SimpleClassFinder;
+
+use ReflectionClass;
+use RuntimeException;
+use InvalidArgumentException;
+use ReflectionException;
+
+class Finder
+{
+    protected $map = [];
+    protected $defaultNamespace = "global";
+
+    public function __construct()
+    {
+        $this->loadClasses();
+    }
+
+    public function loadClasses()
+    {
+        foreach(get_declared_classes() as $class)
+        {
+            $namespacePieces = explode("\\", $class);
+
+            $this->updateNamespaceMap($namespacePieces, $class);
+        }
+
+        return $this;
+    }
+
+    protected function updateNamespaceMap(array $namespacePieces, $classNamespace)
+    {
+        if (!$namespacePieces) {
+            // register the class in the global namespace
+            $this->map[$this->defaultNamespace][] = $classNamespace;
+            return;
+        }
+
+        // discard last part of the namespace
+        array_pop($namespacePieces);
+
+        $namespaceKey = implode('\\', $namespacePieces);
+        if (!$namespaceKey) {
+            return $this->updateNamespaceMap($namespacePieces, $classNamespace);
+        }
+
+        $this->map[$namespaceKey][] = $classNamespace;
+        return $this->updateNamespaceMap($namespacePieces, $classNamespace);
+    }
+
+    public function loadClassesFrom($path)
+    {
+        $autoloaderClass = "";
+
+        foreach ($this->map['global'] as $class) {
+            if (strpos($class, 'ComposerAutoloaderInit') === 0) {
+                $autoloaderClass = $class; // sample ComposerAutoloaderInite7ea52338f6c93b4dc60b2cee949379c
+                break;
+            }
+        }
+
+        if (!$autoloaderClass) {
+            throw new RuntimeException("The ComposerAutoloaderInit class was not found!");
+        }
+
+        $loader = $autoloaderClass::getLoader();
+
+        // you need to run composer dump-autoload with the flag to optmize it
+        // if your project does not use classmap
+        foreach ($loader->getClassMap() as $classFilename) {
+            if (strpos($classFilename, $path) == false) {
+                continue;
+            }
+
+            require_once $classFilename;
+        }
+
+        return $this->loadClasses();
+    }
+
+    public function getNamespaces()
+    {
+        return array_keys($this->map);
+    }
+
+    public function all()
+    {
+        return $this->map;
+    }
+
+    public function getClassesFromNamespace($namespace = '')
+    {
+        if (!$namespace) {
+            $namespace = $this->defaultNamespace;
+        } else if(!isset($this->map[$namespace])) {
+            throw new InvalidArgumentException("The namespace '$namespace' was not found!");
+        }
+
+        return $this->map[$namespace];
+    }
+
+    public function getClassesThatImplements($baseClass, $namespace = '')
+    {
+        $baseClass = new ReflectionClass($baseClass);
+        $classes = [];
+
+        foreach ($this->getClassesFromNamespace($namespace) as $class) {
+            $reflectionClass = new ReflectionClass($class);
+
+            if ($baseClass->isInterface() && $reflectionClass->implementsInterface($baseClass)) {
+                $classes[] = $class;
+            } else if ($reflectionClass->isSubclassOf($baseClass)) {
+                $classes[] = $class;
+            }
+        }
+
+        return $classes;
+    }
+}
